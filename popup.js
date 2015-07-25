5bf583243f8cec2346e1a5ef9d6ceb7b3e32f06d
Xelnect <dido.jordanov7@gmail.com>
2015-07-25 11:31:36 +0300

    iframes

--- popup.js
+++ popup.js
@@ -1,20 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
- 
-    chrome.tabs.getSelected(null, function(tab) {
-      d = document;
- 
-      var f = d.createElement('form');
-      f.action = 'http://gtmetrix.com/analyze.html?bm';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
-      i.value = tab.url;
-      f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
-    });
-  }, false);
-}, false);
+$(document).ready(function() {
+  var popup = localStorage.popup || 'igoogle';
+
+  var frame = document.createElement('iframe');
+
+  if (popup == 'gmail') {
+    // open gmail gadget
+    $('body').height(500).width(500);
+    frame.setAttribute('src', 'http://www.rememberthemilk.com/services/modules/gmail/');
+  }
+  else if (popup == 'iphone') {
+    // open iphone web app
+    $('body').height(480).width(320);
+    frame.setAttribute('src', 'http://i.rememberthemilk.com/');
+  }
+  else if (popup == 'mobile') {
+    // open mobile web app
+    $('body').height(500).width(500);
+    frame.setAttribute('src', 'http://m.rememberthemilk.com/');
+  }
+  else {
+    // igoogle and default
+    $('body').height(600).width(600);
+    frame.setAttribute('src', 'http://www.rememberthemilk.com/services/modules/googleig/');
+  }
+
+  document.body.appendChild(frame);
+});
